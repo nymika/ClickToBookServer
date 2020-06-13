da50af0cdfd26eefe8be2f9c5b7a6f47473148a3
nymika <p.nymika@gmail.com>
2020-06-13 20:59:48 +0530

    git deploy

--- db/mongoAtlas.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const connection = "mongodb+srv://Nym:mongochikki@click-to-book-cluster-ghs1g.mongodb.net/click-to-book-DB?retryWrites=true&w=majority";
-//mongo "mongodb+srv://click-to-book-cluster-ghs1g.mongodb.net/<dbname>" --username Nym
-mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
-    .then(() => console.log("Database Connected Successfully"))
-    .catch(err => console.log(err));
